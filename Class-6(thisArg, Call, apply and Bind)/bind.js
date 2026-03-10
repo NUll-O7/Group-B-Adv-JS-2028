d1ea1e269c5cf1814d3b45451cb6f817dfbc8fe7
mrinal1224 <iammrinalkc@gmail.com>
2026-03-10 11:22:53 +0530

    Push

---
+++ Class-6(thisArg, Call, apply and Bind)/bind.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("showName");
+console.log;
+
+function greet(city, country) {
+  console.log("Hello " + this.name + " " + city + " " + country);
+}
+
+const user = {
+  name: "steve",
+};
+
+const greetUser = greet.bind(user,"Mumbai", "India" );
+
+console.log(greetUser);
+
+
+button.addEventListener("click", greetUser);
+
+
+// Polyfills for call apply and Bind
